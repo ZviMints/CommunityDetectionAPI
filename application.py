a09a74351b11f7a151a45ac27f9120c9dd9ca696
ZviMints <zvimints@gmail.com>
2020-05-15 15:25:15 +0300

    added session

--- application.py
+++ application.py
@@ -1,16 +1,22 @@
 import logging
 
 from flask import Flask, render_template
+from flask_session import Session
 from flask import jsonify
-
+from tempfile import mkdtemp
 import networkx
 from networkx.readwrite import json_graph
-
 import matplotlib
 import matplotlib.pyplot as plt
 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # Serve the file over http to allow for cross origin requests
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 # Zvi Mints And Eilon Tsadok
 @app.route("/")
@@ -26,13 +32,16 @@ def graph():
 def load(dataset):
     app.logger.info('got ./load request')
 
-    #making G (networkx)
+    # Making G (networkx)
     if dataset == "pan12-sexual-predator-identification-training-corpus-2012-05-01":
         G = networkx.read_multiline_adjlist("./load/train_networkxBeforeRemove.adjlist")
     elif dataset == "pan12-sexual-predator-identification-test-corpus-2012-05-17":
         G = networkx.read_multiline_adjlist("./load/test_networkxBeforeRemove.adjlist")
     else:
-           return jsonify(err="405 Method Not Allowed")
+           return jsonify(err="405 Method Not Allowed - Invalid JSON file name")
+
+    session["dataset"] = dataset
+    app.logger.debug('dataset saved successfully on session')
 
     # Generate picture of networkx
     # networkx.draw(G, node_size=1)
@@ -53,9 +62,46 @@ def load(dataset):
     # write json formatted data
     after = json_graph.node_link_data(G)  # node-link format to serialize
 
-    networkx.draw(G, node_size=3)
-    plt.savefig("./load/results/networkx_after_remove.png")
-    plt.close()
+    # Save after remove graph
+    networkx.write_multiline_adjlist(G, "./load/graph.adjlist")
+    session["G"] = G
+    app.logger.debug('G saved successfully on session')
 
-    app.logger.debug('saved in ./load/results/networkx_after_remove.png and ./load/results/networkx_before_remove.png and returning a JSON format file')
+    # Plotting
+    networkx.draw(G, node_size=3)
+    plt.savefig("./load/networkx_after_remove.png")
+
     return jsonify(before=before, after=after)
+
+#=============================================== embedding route ================================================#
+def saveWalks(walks):
+    f = open("./embedding/walks.txt", "w+")
+    row = 1
+    for sentence in walks:
+        f.write("row %s:    " % str(row))
+        row = row + 1
+        for word in sentence:
+            f.write(word)
+            f.write("  ")
+        f.write("\n")
+
+    session["walks"] = f
+    app.logger.debug('walks saved successfully on session')
+    f.close()
+#=============================================== main embedding route ================================================#
+@app.route("/embedding", methods=['GET'])
+def embedding():
+    if not "dataset" or "G" in session:
+        return jsonify(err="405 Method Not Allowed - Please make /load step first")
+
+    G = session["G"][0]
+    
+     # Precompute probabilities and generate walks
+    node2vec = Node2Vec(G, dimensions=64, walk_length=25, num_walks=10, workers=1)
+    saveWalks(list(node2vec.walks))
+
+    model = node2vec.fit(window=10, min_count=1, batch_words=4)
+    session["mode.wv"] = model.wv
+    app.logger.debug('mode.wv saved successfully on session')
+
+    return jsonify("model& walks saved successfully on path embedding/walks.txt")
