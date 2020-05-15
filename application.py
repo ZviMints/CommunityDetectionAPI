222c8681132baeed604c0edb39de0dd912e9b22b
Eilon Tsadok <37323667+eilon26@users.noreply.github.com>
2020-05-15 15:36:51 +0300

    s

--- application.py
+++ application.py
@@ -1,21 +1,17 @@
-import logging
-
-from flask import Flask, render_template
+from flask import Flask, session
 from flask_session import Session
 from flask import jsonify
 from tempfile import mkdtemp
 import networkx
 from networkx.readwrite import json_graph
-import matplotlib
 import matplotlib.pyplot as plt
 
+app = Flask(__name__)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Serve the file over http to allow for cross origin requests
-app = Flask(__name__)
 app.secret_key = "super secret key"
 
 # Zvi Mints And Eilon Tsadok
